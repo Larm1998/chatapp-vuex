5393b467f7e6c32ae9f675cc3ec27ea99d229882
Luis Rangel <luis.rangel@intechideas.com>
2022-09-23 12:00:51 -0500

    Messages

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import { createStore } from 'vuex'
 import profile from '@/store/modules/profile'
 import channels from '@/store/modules/channels'
+import messages from '@/store/modules/messages'
 import { COMMIT_SET_STATUS } from '@/common/mutation-types.js'
 
 const store = createStore({
@@ -21,6 +22,7 @@ const store = createStore({
   modules: {
     profile,
     channels,
+    messages,
   }
 })
 
